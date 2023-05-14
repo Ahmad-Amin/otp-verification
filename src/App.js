642addced96958c9796cc5cec157eda33ff0e9b6
Wajeeh Ahsan <wajeeh.ahsan3@gmail.com>
2023-05-14 18:20:14 +0500

    Design fix and error handling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import "react-phone-input-2/lib/style.css"
 import {auth} from './firebase.config.js'
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import { toast, Toaster } from 'react-hot-toast';
+import "./App.css"
+
+import Card from './components/card'
 
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
   const [phone, setPhone] = useState('')
   const [showOtp, setShowOtp] = useState(false);
   const [user, setUser] = useState(null);
+  const [correctOtp, setCorrectOtp] = useState(true)
 
 
   const onCaptchVertify = () => {
@@ -25,8 +29,7 @@ function App() {
           onSignup()
         },
         'expired-callback': () => {
-          // Response expired. Ask user to solve reCAPTCHA again.
-          // ...
+          
         }
       }, auth);
     }
@@ -44,8 +47,6 @@ function App() {
       setLoading(false);
       setShowOtp(true);
       toast.success('Otp sended successfully!');
-
-
     }).catch((error) => {
       console.log(error);
       setLoading(false);
@@ -61,68 +62,46 @@ function App() {
       setLoading(false);
     }).catch((err) => {
       setLoading(false);
+      toast.error('Incorrect OTP..!');
       console.log(err);
     })
   }
 
   return (
-    <section className="bg-emerald-500 flex items-center justify-center">
+    <section className="flex items-center justify-center">
       <div>
         <Toaster toastOptions={{duration: 4000}} />
         <div id="recaptcha-container"></div>
 
         { user ? (
-          <h2 className='text-center text-white font-medium text-3xl mb-6'>
+          <h2 className='text-center font-medium text-3xl mb-6'>
             OTP verified Succesfully
           </h2>
         ) : (
-          <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
+          <div className=" flex flex-col gap-4 rounded-lg p-4">
             <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6"> Welcome To <br/> CODE A PROGRAM</h1>
 
             {showOtp ? (
-              <>
-                <label htmlFor="phone" className="font-bold text-xl text-white text-center">Enter your OTP</label>
-                <OtpInput 
-                  value={otp} 
-                  onChange={setOtp} 
-                  autoFocus 
-                  OTPLength={6} 
-                  otpType="number" 
-                  disabled={false} 
-                  secure 
-                  className="otp-container"
+                <Card 
+                  inputContent={otp} 
+                  setInputContent={setOtp} 
+                  onSignup={onSignup} 
+                  loading={loading}
+                  type={false} 
+                  onOtpVerify={onOtpVerify}
                 />
-                <button onClick={onOtpVerify} className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-white rounded">
-                  { loading && <CgSpinner size={20} className="mt-1 animate-spin"  /> }
-                  
-                  <span>Verify OTP</span>
-                </button>
-              </>
             ) : (
-              <>
-                <label htmlFor="phone" className="font-bold text-xl text-white text-center">Verify your phone Number</label>
-                <PhoneInput 
-                  country={'pk'}
-                  value={phone}
-                  onChange={setPhone}
-                />
-                
-                <button onClick={onSignup} className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-white rounded">
-                  { loading && <CgSpinner size={20} className="mt-1 animate-spin"  /> }
-                  
-                  <span>Send code via SMS</span>
-                </button>
-              </>
+              <Card 
+                inputContent={phone} 
+                setInputContent={setPhone} 
+                onSignup={onSignup} 
+                loading={loading}
+                type={true} 
+              />
             )
-
             }
           </div>
         )}
-
-        
-
-        
-
       </div>
     </section>  
   );
